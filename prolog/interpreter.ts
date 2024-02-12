01a34fff08d96d5f235fc4fc25011d911790966d
Ethan Niser <100045248+ethanniser@users.noreply.github.com>
2024-02-12 20:50:22 +0000

    remove undeed impl detail for now

--- prolog/interpreter.ts
+++ prolog/interpreter.ts
@@ -5,11 +5,7 @@ interface Interpreter {
 }
 
 class InterpreterImpl implements Interpreter {
-  private knowledgeBase: AST.KnowledgeBase;
-
-  constructor(knowledgeBase: AST.KnowledgeBase) {
-    this.knowledgeBase = knowledgeBase;
-  }
+  constructor(knowledgeBase: AST.KnowledgeBase) {}
 
   public query(query: AST.Query): boolean {
     throw new Error("Not implemented");
