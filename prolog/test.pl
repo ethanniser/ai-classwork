e1abcf57023b5725cb87ba3a6e68d57a9ac57fa7
Ethan Niser <ethan@niser.com>
2024-03-06 00:44:38 +0000

    added more complex grandparent example in test.pl

--- prolog/test.pl
+++ prolog/test.pl
@@ -5,3 +5,17 @@ f(W) :- foo(W).
 sibling(bob, paul).
 sibling(bob, fred).
 sibling(fred, bob).
+
+
+parent(albert, bob).
+parent(albert, betsy).
+parent(albert, bill).
+
+parent(alice, bob).
+parent(alice, betsy).
+parent(alice, bill).
+
+parent(bob, carl).
+parent(bob, charlie).
+
+grandparent(C, M, D) :- parent(M, C), parent(D, C).
