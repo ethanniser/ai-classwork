a0d053325002c2e46eb2e38a8676f775f9852a81
Ethan Niser <100045248+ethanniser@users.noreply.github.com>
2024-04-03 15:23:46 +0000

    init griddler app

--- parsing/index.ts
+++ parsing/index.ts
@@ -2,15 +2,32 @@ import * as ohm from "ohm-js";
 
 const grammar = await Bun.file("./parsing/prolog.ohm").text();
 const parser = ohm.grammar(grammar);
-const s = parser.createSemantics();
-s.addOperation("idk", {
+const semantics = parser.createSemantics();
+/*
+Statement = (Rule | Functor) "."
+  
+  Functor = (Identifier | Variable) ("(" ListOf<Functor, ",">  ")")?
+  Rule = Functor ":-" ListOf<Functor, ",">
     
-})
+  Identifier = lower letter*
+  Variable = upper letter*
+*/
+semantics.addOperation("idk", {
+  Statement(one: any, two: any) {
+    return one.idk();
+  },
+  Functor(one: any, two: any, three: any, four: any) {
+    // console.log([one, two, three, four]);
+  },
+  Rule(one: any, two: any, three: any) {
+    // console.log([one, two, three]);
+  },
+});
 
 const input = `foo(bar).`;
 const match = parser.match(input);
 
 if (match.succeeded()) {
-  const result = s(match);
+  const result = semantics(match).idk();
   console.log(result);
 }
